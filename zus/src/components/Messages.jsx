81f1e83ec22140371b44a85324719a0468d8fe0b
AdieDev0 <devutella@gmail.com>
2025-04-10 10:46:49 +0800

    Enhance Messages component with improved shadow effect and cursor pointer

--- zus/src/components/Messages.jsx
+++ zus/src/components/Messages.jsx
@@ -3,7 +3,7 @@ import { TbMessage } from "react-icons/tb";
 
 const Messages = () => {
   return (
-    <div className="bottom-5 right-5 fixed bg-zusPrimary p-4 rounded-full shadow-lg z-50">
+    <div className="bottom-5 right-5 fixed bg-zusPrimary p-4 rounded-full shadow-xl z-50 cursor-pointer">
       <TbMessage className="text-white size-8" />
     </div>
   );
