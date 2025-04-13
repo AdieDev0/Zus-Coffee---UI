89aec2cd6ef19305bf652e5e92d9599ec2e8417e
AdieDev0 <devutella@gmail.com>
2025-04-13 15:47:19 +0800

    Add Footer component to App for improved user engagement

--- zus/src/App.jsx
+++ zus/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Messages from "./components/Messages";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
--- zus/src/components/Footer.jsx
+++ zus/src/components/Footer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <div className="bg-blue-100">
+        <h1>Follow us on</h1>
+        <div className="flex justify-center gap-4 mt-5">
+          <FaSquareFacebook />
+          <FaInstagram />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
