4d8ac78668fc8bdf2ca40af102a175d7f9b4e71e
AdieDev0 <devutella@gmail.com>
2025-04-13 15:50:00 +0800

    Refactor Footer component for improved styling and accessibility

--- zus/src/components/Footer.jsx
+++ zus/src/components/Footer.jsx
@@ -4,11 +4,13 @@ import { FaInstagram } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div>
-      <div className="bg-blue-100">
-        <h1>Follow us on</h1>
+      <div className="bg-blue-50 py-10">
+        <h1 className="text-center text-lg font-bold text-black/80">
+          Follow us on
+        </h1>
         <div className="flex justify-center gap-4 mt-5">
-          <FaSquareFacebook />
-          <FaInstagram />
+          <FaInstagram className="size-7 text-orange-600 hover:scale-110 duration-200 cursor-pointer" />
+          <FaSquareFacebook className="size-7 text-blue-600 hover:scale-110 duration-200 cursor-pointer" />
         </div>
       </div>
     </div>
